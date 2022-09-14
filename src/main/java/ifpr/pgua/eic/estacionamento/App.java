09736327ecd06ee903101104e7575fef0f65ffff
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2022-09-14 08:42:40 -0300

    finalizado

--- src/main/java/ifpr/pgua/eic/estacionamento/App.java
+++ src/main/java/ifpr/pgua/eic/estacionamento/App.java
@@ -1,8 +1,11 @@
 package ifpr.pgua.eic.estacionamento;
 
+import ifpr.pgua.eic.estacionamento.controllers.JanelaCadastro;
+import ifpr.pgua.eic.estacionamento.controllers.JanelaLista;
 import ifpr.pgua.eic.estacionamento.controllers.JanelaPrincipal;
 import ifpr.pgua.eic.estacionamento.models.Estacionamento;
 import ifpr.pgua.eic.estacionamento.utils.BaseAppNavigator;
+import ifpr.pgua.eic.estacionamento.utils.ScreenRegistryFXML;
 import ifpr.pgua.eic.estacionamento.utils.ScreenRegistryNoFXML;
 
 /**
@@ -11,6 +14,14 @@ import ifpr.pgua.eic.estacionamento.utils.ScreenRegistryNoFXML;
 public class App extends BaseAppNavigator {
 
     private Estacionamento estacionamento;
+
+    @Override
+    public void init() throws Exception {
+        // TODO Auto-generated method stub
+        super.init();
+
+        estacionamento = new Estacionamento("Esta", "1122");
+    }
 
     @Override
     public String getHome() {
@@ -25,6 +36,7 @@ public class App extends BaseAppNavigator {
     @Override
     public void registrarTelas() {
         registraTela("PRINCIPAL", new ScreenRegistryNoFXML(o->new JanelaPrincipal().getRoot()));
-        
+        registraTela("CADASTRO",new ScreenRegistryFXML(App.class, "cadastro.fxml", o->new JanelaCadastro(estacionamento)));
+        registraTela("LISTA",new ScreenRegistryFXML(App.class, "lista.fxml", o->new JanelaLista(estacionamento)));
     }
 }
